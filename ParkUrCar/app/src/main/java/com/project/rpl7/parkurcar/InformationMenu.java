9bff814ddb0f0aaded4c2dd80e8f7fc20719dcc8
Dany Rizkiawan <danyrizkiawan@gmail.com>
2017-10-31 06:08:45 +0700

    Android Code

---
+++ ParkUrCar/app/src/main/java/com/project/rpl7/parkurcar/InformationMenu.java
@@ -0,0 +1,26 @@
+package com.project.rpl7.parkurcar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dany Rizkiawan on 10/29/2017.
+ */
+
+public class InformationMenu extends Fragment{
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.information_menu, container, false);
+    }
+}
---
+++ ParkUrCar/app/src/main/java/com/project/rpl7/parkurcar/ReservationMenu.java
@@ -0,0 +1,26 @@
+package com.project.rpl7.parkurcar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dany Rizkiawan on 10/29/2017.
+ */
+
+public class ReservationMenu extends Fragment{
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.reservation_menu, container, false);
+    }
+}
